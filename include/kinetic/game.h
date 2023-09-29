fe5d4143e70704c5ab6ea73b1f2ee0b23ca318fd
Kasper de Vries <89792927+KasperJSdeVries@users.noreply.github.com>
2023-09-29 12:50:02 +0200

    Add input to entry

--- include/kinetic/game.h
+++ include/kinetic/game.h
@@ -6,4 +6,17 @@ void game_cleanup();
 
 void game_loop();
 
+typedef enum key {
+    KEY_ESCAPE,
+    KEY_A,
+    KEY_D,
+    KEY_S,
+    KEY_W,
+    KEY_SPACE,
+} key;
+
+void game_input_callback(key key);
+
+void exit_game();
+
 #endif//KINETIC_GAME_H
--- src/entry.c
+++ src/entry.c
@@ -7,36 +7,41 @@
 #include <stdio.h>
 
 void key_callback(GLFWwindow *window, int key, int scancode, int action, int modifiers) {
-    if (key == GLFW_KEY_ESCAPE) {
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
-    }
-
     if (action == GLFW_PRESS) {
-        if (darray_length(snake_block_list) != 0) {
-            square *first = snake_block_list[0];
-            if (key == GLFW_KEY_A) {
-                square_set_direction(first, DIR_LEFT);
-            }
-            if (key == GLFW_KEY_D) {
-                square_set_direction(first, DIR_RIGHT);
-            }
-            if (key == GLFW_KEY_W) {
-                square_set_direction(first, DIR_UP);
-            }
-            if (key == GLFW_KEY_S) {
-                square_set_direction(first, DIR_DOWN);
-            }
-        }
-        if (key == GLFW_KEY_SPACE) {
-            add_tail();
+        switch (key) {
+            case GLFW_KEY_ESCAPE:
+                game_input_callback(KEY_ESCAPE);
+                break;
+            case GLFW_KEY_SPACE:
+                game_input_callback(KEY_SPACE);
+                break;
+            case GLFW_KEY_A:
+                game_input_callback(KEY_A);
+                break;
+            case GLFW_KEY_D:
+                game_input_callback(KEY_D);
+                break;
+            case GLFW_KEY_S:
+                game_input_callback(KEY_S);
+                break;
+            case GLFW_KEY_W:
+                game_input_callback(KEY_W);
+                break;
+            default:
+                break;
         }
     }
 }
 
+GLFWwindow *window;
+
+unsigned int vertexArray;
+unsigned int vertexBuffer;
+
 int main(int argc, char *argv[]) {
     if (!glfwInit()) return -1;
 
-    GLFWwindow *window = glfwCreateWindow(800, 800, "Snake", NULL, NULL);
+    window = glfwCreateWindow(800, 800, "Snake", NULL, NULL);
     if (window == NULL) {
         fprintf(stderr, "Error: Could not create a window!\n");
         glfwTerminate();
@@ -83,4 +88,8 @@ int main(int argc, char *argv[]) {
     game_cleanup();
     glfwTerminate();
     return 0;
+}
+
+void exit_game() {
+    glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
