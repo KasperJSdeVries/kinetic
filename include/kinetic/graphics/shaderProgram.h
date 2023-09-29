7821444aa3b5c9623614a8b262a3e4b6856de5a7
Kasper de Vries <89792927+KasperJSdeVries@users.noreply.github.com>
2023-09-29 11:59:43 +0200

    Add content

---
+++ include/kinetic/graphics/shaderProgram.h
@@ -0,0 +1,20 @@
+#ifndef KINETIC_SHADERPROGRAM_H
+#define KINETIC_SHADERPROGRAM_H
+
+#include "cglm/vec3.h"
+
+typedef struct ShaderProgram ShaderProgram;
+
+ShaderProgram *shaderProgram_create();
+void shaderProgram_destroy(ShaderProgram *program);
+
+void shaderProgram_attach(ShaderProgram *this, const char *file_name, unsigned int shader_type);
+void shaderProgram_link(ShaderProgram *this);
+void shaderProgram_use(ShaderProgram *this);
+
+void shaderProgram_add_uniform(ShaderProgram *this, const char *varName);
+void shaderProgram_set_float(ShaderProgram *this, const char *varName, float value);
+void shaderProgram_set_vec3(ShaderProgram *this, const char *varName, vec3 value);
+void shaderProgram_set_vec4(ShaderProgram *this, const char *varName, vec4 value);
+
+#endif//KINETIC_SHADERPROGRAM_H
